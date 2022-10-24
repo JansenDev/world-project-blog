bdfecb7d6dad27600c91520ef64a3dff6c2e4459
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-10-23 21:01:53 -0500

    CHANGE: maquetado de home

--- src/shared/components/layout/Init/Init.tsx
+++ src/shared/components/layout/Init/Init.tsx
@@ -7,11 +7,13 @@ function InitDefault() {
             <header className='m-auto shadow w-full mb-[2px]'>
                 <Shared.Header />
             </header>
-            <main className='w-5/6 mx-auto '>
+            <main className='bg-white'>
+                <section className='w-5/6 mx-auto pb-8'>
                 <Outlet />
+                </section>
             </main>
             <footer className='w-full'>
-                <hr className=" sm:border-gray-200 sm:mx-auto dark:border-gray-700 mt-8 bg-slate-800  w-full" />
+                <hr className=" sm:border-gray-200 sm:mx-auto dark:border-gray-700 bg-slate-800  w-full" />
                 <Shared.Footer />
             </footer>
         </>
