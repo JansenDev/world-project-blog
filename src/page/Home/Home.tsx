61fa054049f1bd23b8c97086033ac0234cca365d
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-10-23 21:05:55 -0500

    CHANGE(Home.tsx): shadow-sm to mobile

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -6,7 +6,7 @@ function Home() {
       <div className="h-[calc(40vh-76px)] sm:h-[calc(60vh-76px)] bg-[url('@assets/images/background.webp')] bg-no-repeat bg-cover bg-top relative rounded-md shadow-sm">
         <div
           className="text-sm md:text-3xl xl:text-4xl uppercase font-semibold absolute sm:bottom-9 bg-transparent bg-slate-50
-        px-4 py-2 rounded-r-lg shadow-md sm:w-max font-elsie bottom-0 right-0 left-0 text-center">Bienvenidos</div>
+        px-4 py-2 rounded-r-lg shadow-sm sm:shadow-md sm:w-max font-elsie bottom-0 right-0 left-0 text-center">Bienvenidos</div>
       </div>
       <section className="pt-5 sm:pt-8 sm:text-xl lg:pt-12 md:text-2xl md:w-5/6 xl:w-2/3 md:mx-auto text-justify">Somos un grupo que se unió para traducir novelas ligeras, este trabajo que realizamos es porque nos gusta las novelas, que las realizamos en nuestro tiempo,
         además de brindarles a los fanáticos de este género la oportunidad de disfrutar de estas historias.</section>
