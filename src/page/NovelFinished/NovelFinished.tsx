9506c73ca703f38e0d33e4c8e45107585383d9a0
JHONATAN JANSSEN SEGURA GALINDO <jsegura-e@ripley.com.pe>
2022-10-27 21:18:30 -0500

    fix(NovelFinished): responsive

--- src/page/NovelFinished/NovelFinished.tsx
+++ src/page/NovelFinished/NovelFinished.tsx
@@ -3,7 +3,7 @@ import Page from "..";
 
 function NovelFinished() {
     return (
-        <div className="h-[calc(100vh-227px)]">
+        <div className="h-[calc(100%-219px)] xs:h-[calc(100vh-219px)]md:h-[calc(100%-219px)] lg:h-[calc(100vh-227px)]">
             <Page.NovelActives status="Finalizado" />
         </div>
     );
